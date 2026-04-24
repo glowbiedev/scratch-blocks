d1970edbddf28464d448330673e7fbe7a8fb56a1
GlowbieDev <glowbiedev@gmail.com>
2026-04-24 11:04:25 +0700

    change AI area name

--- msg/messages.js
+++ msg/messages.js
@@ -293,6 +293,7 @@ Blockly.Msg.SOUND_VOLUME = 'volume';
 Blockly.Msg.SOUND_RECORD = 'record...';
 
 // Category labels
+Blockly.Msg.CATEGORY_AI = 'AI';
 Blockly.Msg.CATEGORY_MOTION = 'Motion';
 Blockly.Msg.CATEGORY_LOOKS = 'Looks';
 Blockly.Msg.CATEGORY_SOUND = 'Sound';
